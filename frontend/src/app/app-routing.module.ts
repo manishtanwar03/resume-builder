909901c2b5fcdc403a3a975f7478fa4ef26e9110
Dhruv Miglani <dhruvmiglani.23@gmail.com>
2019-06-28 12:01:50 +0530

    home page and footer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
   { path:'',redirectTo:'/dashboard',pathMatch:'full'},
+  {path:'home',component:HomePageComponent}
 ];
 
 @NgModule({
