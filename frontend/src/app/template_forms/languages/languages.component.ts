1b49c7eb61e1531cf1cf4816feb71a53f397459f
Dhruv Miglani <dhruvmiglani.23@gmail.com>
2019-07-04 15:56:25 +0530

    Local Storage added to forms

--- frontend/src/app/template_forms/languages/languages.component.ts
+++ frontend/src/app/template_forms/languages/languages.component.ts
@@ -9,21 +9,35 @@ export class LanguagesComponent implements OnInit {
   public languages=[];
   constructor() { }
 
+  private getLanguages()
+  {
+    let localStorageItem=JSON.parse(localStorage.getItem('languages'));
+    return localStorageItem==null? []  :localStorageItem;
+  }
+
+  private setLocalStorageLanguages(){
+    localStorage.setItem('languages',JSON.stringify(this.languages));
+  }
+
   ngOnInit() {
+    this.languages = this.getLanguages();
   }
 
   onEnter(language){
     language = language.trim().toLowerCase();
     if(language && !this.languages.find((entry)=>entry.language==language)){
       this.languages.push({'language':language,'value':100});
+      this.setLocalStorageLanguages();
     }
   }
 
   removeMe(id){
     this.languages.splice(id,1);
+    this.setLocalStorageLanguages();
   }
 
   setProficiency(id,value){
     this.languages[id].value = Number(value);
+    this.setLocalStorageLanguages();
   }
 }
