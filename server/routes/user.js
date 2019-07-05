e66e94828136272ff09f5a7bdeecfa60fdfc898c
abhishek-kumar-gupta <abhishek.24748@lpu.co.in>
2019-07-05 16:38:43 +0530

    Authentication started

---
+++ server/routes/user.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../controllers/user');
+
+router.get('/', (req,res) => {
+res.send('From API route');    
+})
+// router.route('/:id').
+// put(User.updateUser);
+
+router.route('/:id').
+delete(User.deleteUser);
+
+
+router.route('/register').
+post(User.addUser);
+
+router.route('/login').
+post(User.authUser);
+
+
+
+module.exports = router;
