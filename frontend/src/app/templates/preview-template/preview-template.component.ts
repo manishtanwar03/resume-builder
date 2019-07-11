dd4bbdfd9bdae41fab03f7f5e4831ce2fa4f3ca1
Manish Tanwar <manish@localhost.localdomain>
2019-07-11 02:48:43 -0400

    data flow enabled in templates

--- frontend/src/app/templates/preview-template/preview-template.component.ts
+++ frontend/src/app/templates/preview-template/preview-template.component.ts
@@ -9,28 +9,12 @@ import { DataService } from 'src/app/services/data.service';
 export class PreviewTemplateComponent implements OnInit {
   @Input() filter:String="#479099";
 
-  basicInformation=null;
-  skills=null;
-  languages=null;
-  interests=null;
-  workHistory=null;
-  projects=null;
-  education=null;
-data="";
+  resume = {};
+
   constructor(private dataService:DataService) { }
 
   ngOnInit() {
+    this.resume = this.dataService.getData(false);
   }
   
-  ngDoCheck(){
-  this.basicInformation = JSON.parse(localStorage.getItem('basicInformation'));
-  this.skills = JSON.parse(localStorage.getItem('skills'));
-  this.languages = JSON.parse(localStorage.getItem('languages'));
-  this.interests = JSON.parse(localStorage.getItem('interests'));
-  this.filter = JSON.parse(localStorage.getItem('filter'));
-  this.workHistory = JSON.parse(localStorage.getItem('workHistory'));
-  this.projects = JSON.parse(localStorage.getItem('projects'));
-  this.education = JSON.parse(localStorage.getItem('education'));
-  }
-
 }
