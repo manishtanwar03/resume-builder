11c73d20645092052c6b438e7a3df99dc40c9e41
Manish Tanwar <manish@localhost.localdomain>
2019-07-11 09:57:45 -0400

    form templates working, combining with remote storage remaining

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../controllers/user');
 
-
-
 router.route('/register').
 post(User.addUser);
 
 router.route('/login').
 post(User.authUser);
-
 
 module.exports = router;
