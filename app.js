d8de249ed9eb29871b6708ab9691348e00cc4958
Manish Tanwar <manish@localhost.localdomain>
2019-08-01 05:48:19 -0400

    cors removed

--- app.js
+++ app.js
@@ -2,19 +2,13 @@ const express = require('express'); //1.starting  setting up express server
 const mongoose = require('mongoose');
 const resume = require('./routes/resume');
 const user = require('./routes/user'); //2   telling server to use user route 
-const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-};
+// const cors = require('cors');
 
 const port = process.env.PORT // port server will listen on
 
 const app = express(); //1.instance of express
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors());
 
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }) // this returns promise
     .then(() => console.log("Connected to Mongodb"))
